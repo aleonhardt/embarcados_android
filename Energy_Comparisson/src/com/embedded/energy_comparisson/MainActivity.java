287f99bfaf2a8fe7dc6f6d91e14a2961a3840908
lhreinicke <lhreinicke@hotmail.com>
2013-10-25 01:41:16 -0200

    Algoritmo recursivo e iterativo para binary search com array de inteiros.

--- Energy_Comparisson/src/com/embedded/energy_comparisson/MainActivity.java
+++ Energy_Comparisson/src/com/embedded/energy_comparisson/MainActivity.java
@@ -1,22 +1,72 @@
 package com.embedded.energy_comparisson;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.view.Menu;
-
-public class MainActivity extends Activity {
-
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity extends Activity
+{
+	public int midpoint(int n1, int n2)
+	{
+		return (n1+n2)/2;
+	}
+	
+	public int iterativeBinarySearch(int[] array, int key, int min, int max)
+	{
+		while(max >= min)
+		{
+			int mid = midpoint(min, max);
+			
+			if(array[mid] < key)
+			{
+				min = mid+1;
+			}
+			else if(array[mid] > key)
+			{
+				max = mid-1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+	public int recursiveBinarySearch(int[] array, int key, int min, int max)
+	{
+		if(max < min)
+		{
+			return -1;
+		}
+		else
+		{
+			int mid = midpoint(min, max);
+			
+			if(array[mid] > key)
+			{
+				return recursiveBinarySearch(array, key, min, mid-1);
+			}
+			else if(array[mid] < key)
+			{
+				return recursiveBinarySearch(array, key, mid+1, max);
+			}
+			else
+			{
+				return mid;
+			}
+		}
+	}
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		int[] array = {0,1,2,3,4,5,6,7,8};
+		TextView tv = new TextView(this);
+		
+		tv.setText(Integer.toString(recursiveBinarySearch(array, 2, 0, 8)));
+		setContentView(tv);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
+} 
