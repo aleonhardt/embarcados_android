3faa27ef34b6c2395ab7c33de0a87f9ecfb9e643
aleonhardt <ale.lionheart@gmail.com>
2013-12-06 18:08:26 -0200

    faltou

--- GyroMusic/gen/com/example/gyromusic/R.java
+++ GyroMusic/gen/com/example/gyromusic/R.java
@@ -26,7 +26,8 @@ public final class R {
         public static final int music_note=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080002;
+        public static final int buttonConnect=0x7f080001;
         public static final int buttonSing=0x7f080000;
     }
     public static final class layout {
